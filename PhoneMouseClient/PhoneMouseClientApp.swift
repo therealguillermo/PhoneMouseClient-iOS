918facc68dc83a43aabcc6ea35210f3c035ae95e
Mike Bugden <mbugden72@gmail.com>
2025-01-14 02:06:18 -0500

    Initial Commit

---
+++ PhoneMouseClient/PhoneMouseClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneMouseClientApp.swift
+//  PhoneMouseClient
+//
+//  Created by Mike Bugden on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PhoneMouseClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhoneMouseClientTests/PhoneMouseClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneMouseClientTests.swift
+//  PhoneMouseClientTests
+//
+//  Created by Mike Bugden on 1/14/25.
+//
+
+import Testing
+@testable import PhoneMouseClient
+
+struct PhoneMouseClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
